c9cbe4f1c033b22f7f38afb298bd69ae0a1d7581
Kame2021 <21cm0108@jec.ac.jp>
2022-11-09 09:16:43 +0900

    Initial Commit

---
+++ ListRefreshable/ListRefreshableApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListRefreshableApp.swift
+//  ListRefreshable
+//
+//  Created by Ryo on 2022/11/09.
+//
+
+import SwiftUI
+
+@main
+struct ListRefreshableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
